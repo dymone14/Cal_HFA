9641a5452257905cc91736c0050ea9c6dc552a18
dymone14 <93107183+dymone14@users.noreply.github.com>
2021-11-29 12:04:20 -0800

    Add files via upload

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,4 +1,18 @@
-SELECT COUNT(Description)
-FROM LoanType;
-
-SELECT * FROM LoanStatus WHERE StatusCode = '510'; 
+SELECT Loan.LoanID, LoanType.LoanCategoryID, StatusCode, LoanStatus.StatusDate
+FROM Loan
+INNER JOIN(
+SELECT LoanStatus.LoanID, LoanStatus.StatusCode, LoanStatus.StatusSequence, LoanStatus.StatusDate
+FROM LoanStatus
+INNER JOIN (
+SELECT LoanStatus.LoanID, MAX(LoanStatus.StatusSequence) AS StatusSequence
+FROM LoanStatus
+GROUP BY LoanID
+) MaxTable ON LoanStatus.LoanID = MaxTable.LoanID AND LoanStatus.StatusSequence = MaxTable.StatusSequence
+) LoanStatus ON Loan.LoanID = LoanStatus.LoanID
+INNER Join(
+SELECT LoanType.LoanCategoryID, LoanType.LoanTypeID
+FROM LoanType
+WHERE LoanType.LoanCategoryID = 1
+) LoanType ON LoanType.LoanTypeID = Loan.LoanTypeID
+WHERE StatusCode = 410
+ORDER BY Loan.LoanID;
