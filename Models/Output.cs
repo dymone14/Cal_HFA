aaeade0c437e3ad1bd82e660aa492b28505d98ea
dymone14 <93107183+dymone14@users.noreply.github.com>
2021-11-29 12:01:16 -0800

    Add files via upload

---
+++ Models/Output.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cal_HFA.Models
+{
+    public class Output
+    {
+        // int LoanID from Loan/ LoanCategoryID from LoanType/ StatusCode/ StatusDate from LoanStatus
+        public int LoanID { get; set; }
+        public int LoanCategoryID { get; set; }
+        public int StatusCode { get; set; }
+        public DateTime StatusDate { get; set; }
+    }
+
+}
